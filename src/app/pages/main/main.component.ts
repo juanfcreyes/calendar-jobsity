6730cc10b491798275c93f02607660a6e2fddf58
juanfcreyes <juanfcreyes@hotmail.com>
2019-10-05 20:01:01 -0500

    fix run problem

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,34 +1,5 @@
-import {
-  Component, OnInit, ChangeDetectionStrategy,
-  ViewChild, TemplateRef
-} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-import { startOfDay, endOfDay, subDays, addDays, endOfMonth,isSameDay,isSameMonth,addHours } from 'date-fns';
-
-import { Subject } from 'rxjs';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import {
-  CalendarEvent,
-  CalendarEventAction,
-  CalendarEventTimesChangedEvent,
-  CalendarView
-} from 'angular-calendar';
-
-
-const colors: any = {
-  red: {
-    primary: '#ad2121',
-    secondary: '#FAE3E3'
-  },
-  blue: {
-    primary: '#1e90ff',
-    secondary: '#D1E8FF'
-  },
-  yellow: {
-    primary: '#e3bc08',
-    secondary: '#FDF1BA'
-  }
-};
 
 @Component({
   selector: 'app-main',
@@ -39,7 +10,6 @@ export class MainComponent implements OnInit {
 
   viewDate = new Date();
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
 }
